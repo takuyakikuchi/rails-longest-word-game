4ef1dec9452c47a047e2ce0fde8aa19ff1ceeff3
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-02-12 14:16:06 +0900

    fixed bug in new controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,21 +2,26 @@ require 'open-uri'
 require 'json'
 
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = Array.new(10) { ('A'..'Z').sample }
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
   def score
-    word = params[:word]
-    if included?(word.upcase, @letters)
-      if english_word?(params[:word]
-        @result = 'Congratulations! #{word} is a valid English word!'
-      else
-        @result = 'Sorry but #{word} does not seem to be a valid English word...'
-      end
-    else
-      @result = 'Sorry but #{word} cannot be built out of #{@letters.split("-")}'
-    end
+    raise
+    # word = params[:word]
+    # if included?(word.upcase, @letters)
+    #   if english_word?(params[:word]
+    #     @result = 'Congratulations! #{word} is a valid English word!'
+    #   else
+    #     @result = 'Sorry but #{word} does not seem to be a valid English word...'
+    #   end
+    # else
+    #   @result = 'Sorry but #{word} cannot be built out of #{@letters.split("-")}'
+    # end
   end
 
   private
