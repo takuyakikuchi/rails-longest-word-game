82565959f75ae7cfc0105225782b920ac925861d
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-02-14 12:57:44 +0900

    WIP test

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -6,4 +6,11 @@ class GamesTest < ApplicationSystemTestCase
     assert test: "New game"
     assert_selector "li", count: 10
   end
+
+  test "Filling the form with a random word, and get a message that the word is not in the grid." do
+    visit new_url
+    word = ('a'..'z').to_a.sample(10).join
+    fill_in "word", with: word
+    click_on "Submit"
+  end
 end
