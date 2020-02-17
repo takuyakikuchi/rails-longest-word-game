631bdd1df2a9745f8c7c979544e8d79b7bf10ef6
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-02-17 11:17:39 +0900

    test done

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -9,8 +9,25 @@ class GamesTest < ApplicationSystemTestCase
 
   test "Filling the form with a random word, and get a message that the word is not in the grid." do
     visit new_url
-    word = ('a'..'z').to_a.sample(10).join
+    letters = find('.letters').text.gsub("\n", " ")
+    word = ('a'..'z').to_a.sample(10).join.upcase
     fill_in "word", with: word
     click_on "Submit"
+
+    within('.result') do
+      assert_text "Sorry but #{word} can't be built out of #{letters}"
+    end
+  end
+
+  test "Filling the form with a one-letter consonant word, and get a message that the word is not a valid English word." do
+    visit new_url
+    letters = find('.letters').text.gsub("\n", " ")
+    word = letters[0]
+    fill_in "word", with: word
+    click_on "Submit"
+
+    within('.result') do
+      assert_text "Sorry but #{word} does not seem to be a valid English word..."
+    end
   end
 end
